2f73b3b011a24c0417359e4042ac034d545846ec
Karl-Einar Bergstrøm <45044399+karleinarb@users.noreply.github.com>
2025-06-13 12:29:24 +0200

    Initialization & dataplatform guides (#1)
    Added landing page and the first dataplatform guide
    
    ---------
    
    Co-authored-by: Rolv Råen <rolv.raaen@gmail.com>

---
+++ README.md
@@ -0,0 +1,23 @@
+<img
+    src="./assets/images/DSB__Logo_Navnetrekk_Hvit.svg"
+    style="
+        background: #1A1A1A;
+        padding: 1em;
+        border-radius: 10px;
+        max-width: 500px;
+    "
+/>
+
+# Introduction
+This is the repository for integration guides/documentation for [DSB](https://www.dsb.no/)
+<br>(Direktoratet for samfunnssikkerhet og beredskap / Direktoratet for samfunnssikkerhet og beredskap / The Norwegian Directorate for Civil Protection)
+
+Here you can find information and examples detailing the avenues we provide for integrations.
+
+## Who is this for?
+
+The code and examples here expects you to have some fundamental programming knowledge, but we try to keep it as simple as possible.
+<br>Our goal is that it should be easy for programmers to follow, and possible for technically inclined users to use with a little effort.
+
+# Guides
+- [Dataplatform](./dataplatform/README.md)
---
+++ dataplatform/examples/lib/deltasharing.py
@@ -0,0 +1,61 @@
+import json
+from delta_sharing import SharingClient, load_as_pandas
+from typing import List
+
+def create_sharing_profile(
+        bearer_token: str,
+        endpoint: str,
+        profile_name: str = "profile",
+    ) -> str:
+    """
+    Create a Delta Sharing profile JSON file.
+    
+    returns the filename of the created profile.
+    """
+
+    # Create a SharingClient instance using the access token
+    profile = {
+        "shareCredentialsVersion": 1,
+        "bearerToken": bearer_token,
+        "endpoint": endpoint
+    }
+
+    profile_filename = f"{profile_name}.json"
+
+    with open(f"{profile_filename}", "w") as f:
+        json.dump(profile, f)
+        
+    return profile_filename
+
+
+def get_table_urls(profile: str, sharing_client: SharingClient):
+    """
+    Convert a list of Delta Sharing tables to resource URLs.
+    """
+
+    tables = sharing_client.list_all_tables()
+    table_list = [f"{profile}#{table.share}.{table.schema}.{table.name}" for table in tables]
+
+    return table_list
+
+def pandas_dump_tables(
+        table_urls: List[str],
+        data_folder: str = "data",
+):
+    """
+    Load Delta Sharing tables as Pandas DataFrames and save them to disk.
+    """
+    import time
+    
+    for index, url in enumerate(table_urls):
+        table_name = url.split('#')[1]
+        start_time = time.time()
+        print(f"Loading table '{table_name}' ({index + 1}/{len(table_urls)})")
+        df = load_as_pandas(url=url)
+        # Create the data folder if it doesn't exist
+        import os
+        os.makedirs(data_folder, exist_ok=True)
+        # Save the DataFrame to a Parquet file
+        df.to_parquet(f"{data_folder}/{table_name}.parquet")
+        end_time = time.time()
+        print(f"Completed in {end_time - start_time:.2f} seconds.\n")
